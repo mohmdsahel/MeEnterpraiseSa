07dee53cbc784122b668b129c25047ec0dc9cd44
Mohamed Sahel <mosahel000@gmail.com>
2025-03-07 19:18:32 +0530

    commit

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -1,8 +1,6 @@
 import Section from "./Section";
 import Heading from "./Heading";
-import { pricing } from "../constants";
-import Button from "./Button";
-import { check } from "../assets";
+
 import { Gradient } from "./design/Services";
 
 
@@ -21,45 +19,8 @@ const Pricing = () => {
 
         <div className="relative">
 
-        <div className="flex gap-[1rem] max-lg:flex-wrap">
-      {pricing.map((item) => (
-        <div
-          key={item.id}
-          className="h-auto w-auto max-lg:w-full  px-6 bg-n-8 border border-n-6 rounded-[2rem] lg:w-auto even:py-4 odd:py-8 odd:my-4 [&>h6]:first:text-color-2 [&>h6]:even:text-color-1 [&>h6]:last:text-color-3"
-        >
-          <h6 className="h5 mb-4">{item.title}</h6>
-
-          <p className="body-2  min-h-[4rem] mb-3 text-n-1/50">
-            {item.description}
-          </p>
-          <p>
-            {item.tag}
-          </p>
-
-          <div className="flex items-center h-[5.5rem] mb-6">
-            {item.price && (
-              <>
-                <div className="h3">$</div>
-                <div className="text-[5.5rem] leading-none font-bold">
-                  {item.price}
-                </div>
-              </>
-            )}
-          </div>
-
-          <Button
-            className={item.price ?  "w-full  mt-16" : "w-full " }
-            href={ "https://meenterpriseai.com/sa/#buyticket"}
-            
-          >
-            {item.price ? "+ADD" : "coming soon"}
-          
-          </Button>
-
-        
-        </div>
-      ))}
-    </div>                                       
+        <iframe id="ts-iframe" src="https://www.townscript.com/v2/widget/middle-east-enterprise-ai-analytics-summit-2025-001441/booking" frameborder="0" height="800" width="100%"></iframe>
+        <link rel="stylesheet" href="https://www.townscript.com/static/Bookingflow/css/ts-iframe.style.css" />
 
         </div>
 
--- src/components/Speakers.jsx
+++ src/components/Speakers.jsx
@@ -1,31 +1,121 @@
 import { motion, useScroll } from "framer-motion";
 import { useRef } from "react";
 import { benefits } from "../constants";
-import Heading from "./Heading";
 import Section from "./Section";
-import { linkedin, spotlight } from "../assets";
+import { linkedin } from "../assets";
+import SectionSvg from "../assets/svg/SectionSvg";
 
+const mergeClasses = (...classes) => classes.filter(Boolean).join(' ');
+
+export const LampContainer = ({
+  children,
+  className
+}) => {
+  return (
+    <div
+      className={mergeClasses(
+        "relative flex min-h-screen flex-col items-center justify-center overflow-hidden bg-n-8 w-full rounded-md z-0",
+        className
+      )}
+    >
+      <div className="relative flex w-full flex-1  items-center justify-center isolate z-0 ">
+        <motion.div
+          initial={{ opacity: 0.5, width: "15rem" }}
+          whileInView={{ opacity: 1, width: "30rem" }}
+          transition={{
+            delay: 0.3,
+            duration: 0.8,
+            ease: "easeInOut",
+          }}
+          style={{
+            backgroundImage: `conic-gradient(var(--conic-position), var(--tw-gradient-stops))`,
+          }}
+          className="absolute inset-auto right-1/2 h-56 overflow-visible w-[30rem] bg-gradient-conic from-cyan-500 via-transparent to-transparent text-white [--conic-position:from_70deg_at_center_top]"
+        >
+          <div className="absolute w-[100%] left-0 bg-n-8 h-40 bottom-0 z-20 [mask-image:linear-gradient(to_top,white,transparent)]" />
+          <div className="absolute w-40 h-[100%] left-0 bg-n-8 bottom-0 z-20 [mask-image:linear-gradient(to_right,white,transparent)]" />
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0.5, width: "15rem" }}
+          whileInView={{ opacity: 1, width: "30rem" }}
+          transition={{
+            delay: 0.3,
+            duration: 0.8,
+            ease: "easeInOut",
+          }}
+          style={{
+            backgroundImage: `conic-gradient(var(--conic-position), var(--tw-gradient-stops))`,
+          }}
+          className="absolute inset-auto left-1/2 h-56 w-[30rem] bg-gradient-conic from-transparent via-transparent to-cyan-500 text-white [--conic-position:from_290deg_at_center_top]"
+        >
+          <div className="absolute w-40 h-[100%] right-0 bg-n-8 bottom-0 z-20 [mask-image:linear-gradient(to_left,white,transparent)]" />
+          <div className="absolute w-[100%] right-0 bg-n-8 h-40 bottom-0 z-20 [mask-image:linear-gradient(to_top,white,transparent)]" />
+        </motion.div>
+        <div className="absolute top-1/2 h-48 w-full translate-y-12 scale-x-150 bg-n-8 blur-2xl"></div>
+        <div className="absolute top-1/2 z-50 h-48 w-full bg-transparent opacity-10 backdrop-blur-md"></div>
+        <div className="absolute inset-auto z-50 h-36 w-[28rem] -translate-y-1/2 rounded-full bg-cyan-500 opacity-50 blur-3xl"></div>
+        <motion.div
+          initial={{ width: "8rem" }}
+          whileInView={{ width: "16rem" }}
+          transition={{
+            delay: 0.3,
+            duration: 0.8,
+            ease: "easeInOut",
+          }}
+          className="absolute inset-auto z-30 h-36 w-64 -translate-y-[6rem] rounded-full bg-cyan-400 blur-2xl"
+        ></motion.div>
+        <motion.div
+          initial={{ width: "15rem" }}
+          whileInView={{ width: "30rem" }}
+          transition={{
+            delay: 0.3,
+            duration: 0.8,
+            ease: "easeInOut",
+          }}
+          className="absolute inset-auto z-50 h-0.5 w-[30rem] -translate-y-[7rem] bg-cyan-400"
+        ></motion.div>
+
+        <div className="absolute inset-auto z-40 h-44 w-full -translate-y-[12.5rem] bg-n-8"></div>
+      </div>
+
+      <div className="relative z-50 flex -translate-y-80 flex-col items-center px-5">
+        {children}
+      </div>
+    </div>
+  );
+};
 
 export default function Speaker() {
   const containerRef = useRef(null);
 
   return (
-    <Section crosses id="speakers" className="h-[100vh] overflow-hidden">
-      <div className="min-h-screen">
-        <div className="z-50 backdrop-blur-sm "> {/* Reduced margin-bottom space */}
-          <Heading className="text-center" title="Spotlight Speakers" />
-         {/* <img src={spotlight} width={200} height={200} className="items-center" /> */}
-         
+    <Section crosses>
+      <div className="h-screen relative overflow-hidden flex flex-col">
+        <motion.h1
+          initial={{ opacity: 0.5, y: -50 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{
+            delay: 0.3,
+            duration: 0.8,
+            ease: "easeInOut",
+          }}
+          className="mt-8 bg-gradient-to-br from-slate-300 to-slate-500  bg-clip-text text-center text-2xl font-medium tracking-tight text-transparent md:text-4xl relative z-10"
+        >
+          Spotlight Speakers
+        </motion.h1>
+        
+        <div className="absolute inset-0 z-0 h-[50vh]">
+          <LampContainer className="w-full h-full">
+          </LampContainer>
         </div>
-     
-        <div className="w-full">
-          
+
+        <div className="w-full relative z-10 h-[50vh] mt-auto">
           <div
             ref={containerRef}
-            className="relative h-[100vh] overflow-auto overscroll-auto " 
+            className="relative h-full overflow-auto overscroll-auto"
             style={{ scrollbarWidth: "none" }}
           >
-            {benefits.map(({ title, text, imageUrl, link,  }, i) => (
+            {benefits.map(({ title, text, imageUrl, link }, i) => (
               <Card
                 key={title}
                 index={i}
@@ -33,7 +123,6 @@ export default function Speaker() {
                 text={text}
                 link={link}
                 image={imageUrl}
-                
                 containerRef={containerRef}
               />
             ))}
@@ -44,7 +133,7 @@ export default function Speaker() {
   );
 }
 
-function Card({ index, title, text, image, link,  containerRef }) {
+function Card({ index, title, text, image, link, containerRef }) {
   const cardRef = useRef(null);
   const { scrollYProgress } = useScroll({
     target: cardRef,
@@ -58,8 +147,8 @@ function Card({ index, title, text, image, link,  containerRef }) {
       className="w-full max-w-4xl mx-auto px-4 sm:px-6" 
       style={{
         position: "sticky",
-        top: `${index * 3}px`,
-        marginBottom: "calc(60vh + 3rem)", // Added 3rem (48px) of extra space below each card
+        top: `${index * 3 + 25}px`,
+        marginBottom: " 3rem",
       }}
     >
       <motion.div
